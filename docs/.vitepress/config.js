8897ade14c75985e09a7de515c0d50a445b30996
小米夏 <mixia478@outlook.com>
2025-12-28 17:07:10 +0800

    更新 config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -30,6 +30,7 @@ export default {
           { text: '变量与常量', link: '/java/variable' },
           { text: '运算符', link: '/java/operator' },
           { text: '类型转换', link: '/java/type-conversion' },
+          { text: 'Scanner', link: '/java/scanner' },
         ]
       }
     ]
